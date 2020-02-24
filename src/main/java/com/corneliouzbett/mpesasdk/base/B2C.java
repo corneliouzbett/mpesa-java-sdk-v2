5e8dff39d135b09dec12371e8ab16d5531523c69
corneliouz Bett <kbett68@gmail.com>
2020-02-24 13:17:43 +0300

    Added request payment endpoint

--- src/main/java/com/corneliouzbett/mpesasdk/base/B2C.java
+++ src/main/java/com/corneliouzbett/mpesasdk/base/B2C.java
@@ -5,6 +5,7 @@ import com.corneliouzbett.mpesasdk.core.rest.response.MpesaResponse;
 import jdk.nashorn.internal.ir.annotations.Immutable;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 /**
  * Use this API to transact between an M-Pesa short code to a phone number registered on M-Pesa.
@@ -12,5 +13,6 @@ import retrofit2.http.Body;
 @Immutable
 public interface B2C {
 
+	@POST("/mpesa/b2c/v1/paymentrequest")
 	Call<MpesaResponse> requestPayment(@Body PaymentRequest paymentRequest);
 }
