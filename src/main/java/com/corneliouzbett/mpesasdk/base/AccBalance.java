b24c392693a3846755b333f944864a1369508198
corneliouz Bett <kbett68@gmail.com>
2020-02-25 11:21:17 +0300

    Implemented account balance service (till number)

--- src/main/java/com/corneliouzbett/mpesasdk/base/AccBalance.java
+++ src/main/java/com/corneliouzbett/mpesasdk/base/AccBalance.java
@@ -1,7 +1,22 @@
 package com.corneliouzbett.mpesasdk.base;
 
+import com.corneliouzbett.mpesasdk.core.rest.request.AccountBalance;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * Use this API for balance inquiry.
+ */
+@Immutable
 public interface AccBalance {
 
-	AccBalance create();
+	/**
+	 * Use this API to enquire the balance on an M-Pesa BuyGoods (Till Number).
+	 *
+	 * @return {@link retrofit2.Call} of account balance
+	 */
+	@POST("/mpesa/accountbalance/v1/query")
+	AccBalance inquiryBalance(@Body AccountBalance accountBalance);
 
 }
